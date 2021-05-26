a3e4c9e45510e73034ea5fb8b3f3324cd4bfd6ef
tplamondon <tplamondon@tutanota.de>
2021-05-26 13:52:17 -0700

    opengoatdoor chosen

--- simulation.py
+++ simulation.py
@@ -1,16 +1,39 @@
-
+from typing import List
 import random
 
 def getRandomDoorArray():
+    """
+    Create a random door array
+    """
     doors = [False, False, False]
     car = random.randrange(3)
     doors[car] = True
     return doors
 
+def chooseDoor():
+    """
+    Choose a random door
+    """
+    return random.randrange(3)
+
+def openGoatDoor(choiceDoor: int, doors: List[int]):
+    """
+    Choose the door to open that's not the choice
+    """
+    door = -1
+    for x in range(len(doors)):
+        if doors[x] == False and x != choiceDoor:
+            door = x
+    return door
 
 def simulate():
+    """
+    Simulate one thing
+    """
     print("Getting random door")
     doors = getRandomDoorArray()
+    pickedDoor = chooseDoor()
+
 
 def main():
     print("Start of Simulation")
